2515d8d6c178fab3c349b2fe485bb5b060a4a431
Jonathan David Livesay <jonl316@gmail.com>
2019-09-10 15:20:32 -0500

    Updated the files and removed the Pipfile.

--- robot_name.py
+++ robot_name.py
@@ -1,3 +1,14 @@
-class Robot(object):
-    def __init__(self):
+class Robot:
+
+    def __init__(self, name):
+
+        def name = name
+
+    def generate_random_letters():
+        pass
+
+    def generate_random_numbers(self):
+        pass
+
+    def get_name(self):
         pass
